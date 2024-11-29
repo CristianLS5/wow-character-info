ada6c1488a3405bbc9805adea5dd1d36e3c07fef
CristianLS5 <cristianlopezc4@gmail.com>
2024-11-29 18:02:25 +0100

    modify endpoints, add interface

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
-// Development environment
-export const environment = {
+import { Environment } from "../app/interfaces/environment.interface";
+
+export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000/api',
   frontendUrl: 'http://localhost:4200',
+  authCallbackUrl: 'http://localhost:3000/auth/callback'
 };
